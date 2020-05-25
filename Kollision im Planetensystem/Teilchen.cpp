10e99444925101a0cc519101bf231cbdb456ab23
Jonas Brandhoff <Cidsbaum@gmail.com>
2020-05-25 16:02:56 +0200

    Added functions for reading txt files

---
+++ Kollision im Planetensystem/Teilchen.cpp
@@ -0,0 +1,3 @@
+#include "Teilchen.h"
+
+
---
+++ Kollision im Planetensystem/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int gesMasse;
+double relGeschwindigkeit;
+double q;
+int sMin, sMax;
+int dichte;
+double volumen;
+
+/*
+liest alle fuer den Start benoetigten Werte aus einer Datei filename
+
+*/
+void readValuesFromFile(string filename, int &sMin, int &sMax, int &dichte, double& q, int &gesMasse, double &relGeschwindigkeit, double &volumen) {
+
+}
+
+/**
+Erzeugt ein logarithmisches Gitter im Bereich min bis max mit schritte als diskreten abstand
+*/
+ vector<int> createLogSpace(int min, int max, int schritte) {
+
+}
+
+
+ /*
+ INITALIESIERUNG von variablen, einlesen der Daten
+ */
+ void INIT() {
+	 readValuesFromFile("anfangswerte.txt", sMin, sMax, dichte, q, gesMasse, relGeschwindigkeit, volumen);
+
+ }
+
+/*
+Main funktion zum code ausfuehren
+*/
+int main(int argc, char* argv[])
+{
+	//initialisieren
+	INIT();
+
+
+	return 0;
+}
