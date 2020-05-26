9f13f6430d2743c1bc11e7027576bc0941fc0b17
Jonas Brandhoff <Cidsbaum@gmail.com>
2020-05-26 12:37:11 +0200

    added a derivative function

--- Kollision im Planetensystem/main.cpp
+++ Kollision im Planetensystem/main.cpp
@@ -6,6 +6,11 @@
 #include <vector>
 #include <string>
 #include <cmath>
+
+//Boost abhaegigkeiten
+#include <boost/multiprecision/cpp_dec_float.hpp>
+#include <boost/math/constants/constants.hpp>
+
 using namespace std;
 
 double gesMasse;
@@ -61,19 +66,48 @@ void readValuesFromFile(string filename, double &sMin, double&sMax, double&dicht
 }
 
 
+
+
+
+ //--------------------------------------------------------------------------------------------------------------------------------------
+ //------------------------------------------------  MATHE TOOLS ETC  -------------------------------------------------------------------
+ //--------------------------------------------------------------------------------------------------------------------------------------
+
 /**
 Erzeugt ein logarithmisches Gitter im Bereich min bis basis^max mit anzahl schritten. Basis gibt dabei die log scala an mit default=10
 
 */
- vector<double> createLogSpace(double min, double max, int schritte, double basis=10) {
-	 vector<double> logspace;
-	 logspace.reserve(schritte);
-	 const auto exponent = (max - min) / (schritte-1); // bastand zwischen zwei Punkten
-	 for (int i = 0; i < schritte; i++) {
-		 logspace.push_back(pow(basis, i * exponent));
-	 }
-	 return logspace;
+vector<double> createLogSpace(double min, double max, int schritte, double basis = 10) {
+	vector<double> logspace;
+	logspace.reserve(schritte);
+	const auto exponent = (max - min) / (schritte - 1); // bastand zwischen zwei Punkten
+	for (int i = 0; i < schritte; i++) {
+		logspace.push_back(pow(basis, i * exponent));
+	}
+	return logspace;
 }
+
+
+ /*
+ Bildet die Ableitung einer Funktion an der stelle x mit schrittweite dx. Die ableitung konvergiert in 6.Ordnung 
+ */
+template<typename function_type>
+ double derivative(double x, float dx, function_type funktion)
+ {
+	 const double dx1 = dx;
+	 const double dx2 = dx1 * 2;
+	 const double dx3 = dx1 * 3;
+
+	 const double m1 = (funktion(x + dx1) - funktion(x - dx1)) / 2;
+	 const double m2 = (funktion(x + dx2) - funktion(x - dx2)) / 4;
+	 const double m3 = (funktion(x + dx3) - funktion(x - dx3)) / 6;
+
+	 const double fifteen_m1 = 15 * m1;
+	 const double six_m2 = 6 * m2;
+	 const double ten_dx1 = 10 * dx1;
+
+	 return ((fifteen_m1 - six_m2) + m3) / ten_dx1;
+ }
 
 
  /*
@@ -84,6 +118,8 @@ Erzeugt ein logarithmisches Gitter im Bereich min bis basis^max mit anzahl schri
 	 double schritte;
 	 readValuesFromFile(config_filename, sMin, sMax, dichte, q, gesMasse, relGeschwindigkeit, volumen, gitterMax, schritte);
 	
+
+
  }
 
 /*
@@ -94,9 +130,7 @@ int main(int argc, char* argv[])
 	//initialisieren
 	INIT();
 
-	for (auto& x : createLogSpace(2, 10, 100)) {
-	//	cout << x << " ";
-	};
+
 
 	return 0;
 }
